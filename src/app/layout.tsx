15ddf3999223328d97a6574b8e2abd2a2661d7b9
Martin Abou <martinaboumonde@gmail.com>
2025-12-09 19:36:00 +0000

    toujours la même erreur :
    Console Error
    
    A tree hydrated but some attr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;900&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${inter.variable} font-body antialiased`}>
+      <body className={`${inter.variable} font-body antialiased`} suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
